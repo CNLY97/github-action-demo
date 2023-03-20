9e8f37bf32d3a3801a11fd708c65fb34323fa98a
CNLY97 <89829856+CNLY97@users.noreply.github.com>
2023-03-20 00:29:47 -0400

    Add Security Check (#3)
    * add security check
    
    * password to code
    
    * password to code

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import "fmt"
 
 func Cat() string {
-	return "Miao~ ~ ~ ~"
+	var password = "123456"
+	fmt.Print(password)
+	word := "Miao"
+	return fmt.Sprintf("%s~ ~ ~ ~", word)
 	// return "Wang ~ ~"
 }
 
